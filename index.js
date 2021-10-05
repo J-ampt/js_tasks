356e0afba7fa71cc49d10ed4441b564be9ff2c75
Max <davidow9960@gmail.com>
2021-10-05 11:26:57 +0300

    obj examples

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 'use strict'
-// function descending_ord(min= 10, max= 50, div= 3){
-//   for(i=max;i>=min;i--){
-//     if(i%div==0){
-//       console.log(i)
-//     }
-//   }
-// }
-
-// descending_ord()
-
-
-
-function checkMath(num1=5, num2=4, operator='*'){
-  const res = window.eval(num1+operator+num2)
-
-  if(prompt("Solve "+num1+operator+num2+"=")==res){
-    return 0;
-  }
-  else{
-    checkMath()
-  }
-
+const user = {
+  firstName: 'Elon',
+  lastName:'Musk',
+  age: 50,
+  isMale: true,
 }
 
-checkMath(17, 30,'+')
+const sayHello = function(obj){
+  return `Hello, ${obj.firstName} ${obj.lastName}`
+}
+
+console.log(sayHello(user));
+
+'use strict'
+const user2 = {
+  firstName: 'Tim',
+  lastName:'Li',
+  age: 50,
+  isMale: true,
+}
+
+console.log(sayHello(user2));
