55ae1441c68e48cab648f09361b556f30bac0e4f
Max <davidow9960@gmail.com>
2021-10-05 12:04:26 +0300

    last_task

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 'use strict'
-const user = {
-  firstName: 'Elon',
-  lastName:'Musk',
-  age: 50,
-  isMale: true,
+
+
+
+const obj ={};
+obj.prop1 = 12;
+obj['prop2'] =22;
+
+const multiplication = function(min=1,max=10){
+  const table = {};
+  for(let i=min; i<=max; i++){
+    for(let j=1; j<=max; j++){
+      table[`${i} * ${j} = `] = i*j;
+    }
+  }
+  return table;
+}
+console.log(multiplication());
+
+const task = function(str, obj){
+  return obj[str];
 }
 
-const sayHello = function(obj){
-  return `Hello, ${obj.firstName} ${obj.lastName}`
-}
 
-console.log(sayHello(user));
-
-'use strict'
-const user2 = {
-  firstName: 'Tim',
-  lastName:'Li',
-  age: 50,
-  isMale: true,
-}
-
-console.log(sayHello(user2));
+const str = '2 * 3 = ';
+const table = multiplication();
+console.log(task(str,table));
